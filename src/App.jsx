d45fbb70af7193038798add4cc1a759cae6f9527
Keiran Pillman <keiran.pillman@gmail.com>
2023-03-20 18:08:01 -0400

    setting up the AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import DisplayContact from './components/DisplayContact.jsx'
 import UserAvatar from './components/UserAvatar.jsx';
 
 export const MyContext = createContext( "context!" );
+export const AuthContext = createContext('auth');
 
 function App() {
   const [contacts, setContacts] = useState([{
@@ -12,6 +13,7 @@ function App() {
     birthday: "September 9, 1988"
   }])
   const [selectedContact, setSelectedContact] = useState(0)
+  const [loggedin, setLoggedin] = useState(false)
   
   const createNewContact = () => {
     setContacts( [...contacts, { name: '', email: '', birthday: '' }] )
@@ -22,10 +24,13 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>Address Book</h1>
-        <UserAvatar/>
-      </header>
+      <AuthContext.Provider value={ { loggedin, setLoggedin} }>
+
+        <header>
+          <h1>Address Book</h1>
+          <UserAvatar/>
+        </header>
+      </AuthContext.Provider>
       <MyContext.Provider value={ {contacts, setContacts, selectedContact, setSelectedContact} }>
         <aside>
           <h2>Contacts</h2>
--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+import { AuthContext } from "../App";
+
 export default function UserAvatar(props) {
-    // const [state, setState] = useState(0)
+    const { loggedin, setLoggedin } = useContext(AuthContext);
+
     console.log('avatar rendered!', new Date().toLocaleTimeString())
     return (
-        <div>hello React!</div>
+        <div className="auth">
+        {
+            loggedin ?
+            <>
+            <p>NAME</p>
+            <button onClick={() => setLoggedin(false)}>Log Out</button>
+            </>
+            : <button onClick={() => setLoggedin(true)}>Log in</button>
+        }
+        </div>
     )
 
 }
